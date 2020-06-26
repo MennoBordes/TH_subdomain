852c1a134f36ed45f35f07fe55a5f4a1aa1d2248
Menno Bordes <mennobordes@hetnet.nl>
2020-06-26 13:31:37 +0200

    Experimenting with EF for db connection

---
+++ TH_Blazor/Client/ViewModel/DisplayPersonModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TH_Blazor.Client.ViewModel
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "First Name is too long.")]
+        [MinLength(1, ErrorMessage = "First Name is too short.")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Last Name is too long.")]
+        [MinLength(1, ErrorMessage = "Last Name is too short.")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ TH_Blazor/Shared/Models/HoutSoort.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TH_Blazor.Shared.Models
+{
+    [Table("hout_soort")]
+    public class HoutSoort
+    {
+        /// <summary> The unique identifier. </summary>
+        public int Id { get; set; }
+
+        /// <summary> The name of the houtsoort. </summary>
+        public string Name { get; set; }
+
+        /// <summary> The price per (100mm) (10cm) (0.1m) </summary>
+        public decimal Price { get; set; }
+
+
+        //=== Helper functions
+
+        /// <summary> Returns the price of the houtsoort. </summary>
+        public decimal GetPrice()
+        {
+            return Price;
+        }
+    }
+}
