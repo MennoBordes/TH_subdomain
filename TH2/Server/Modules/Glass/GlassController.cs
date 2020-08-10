fcb7cd3a9b954f0b0d2b736164cc87553300de17
Menno Bordes <mennobordes@hetnet.nl>
2020-08-10 18:22:44 +0200

    live// Created controller for Glass

--- TH2/Server/Modules/Glass/GlassController.cs
+++ TH2/Server/Modules/Glass/GlassController.cs
@@ -1,47 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TH2.Server.Modules.Glass
 {
+    using Newtonsoft.Json.Linq;
+    using Shared.Modules.Glass;
+    using Shared.Modules.Glass.Entities;
+
     [Route("api/[controller]")]
     [ApiController]
     public class GlassController : ControllerBase
     {
+        GlassManager gMan;
+
+        public GlassController()
+        {
+            gMan = new GlassManager();
+        }
+
+        #region Glass
+
         // GET: api/<GlassController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        [HttpGet("GetGlass")]
+        public Glass GetGlass([FromBody] JToken json)
         {
-            return new string[] { "value1", "value2" };
+            /*
+            Request
+                {
+                    id: 1,
+                    ventilation: true
+                }             
+            */
+
+            if (json["id"] == null)
+                throw new Exception("No Id specified!");
+
+            int id = json.Value<int>("id");
+            bool ventilation = json.Value<bool>("ventilation");
+
+            return gMan.GetGlass(id, ventilation);
         }
 
-        // GET api/<GlassController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpPost("SaveGlass")]
+        public int SaveGlass([FromBody] Glass glass)
         {
-            return "value";
+            return gMan.SaveGlass(glass);
         }
 
-        // POST api/<GlassController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost("DeleteGlass")]
+        public ActionResult DeleteGlass([FromBody] Glass glass)
         {
+            if (glass == null || glass.Id < 1)
+                return BadRequest();
+
+            gMan.DeleteGlass(glass.Id);
+
+            return Ok();
         }
 
-        // PUT api/<GlassController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        #endregion
+
+        #region Ventilation
+
+        [HttpGet("GetVentilation")]
+        public GlassVentilation GetVentilation([FromBody] int id)
         {
+            if (id < 1)
+                throw new Exception("Invalid Id Specified!");
+
+            return gMan.GetVentilation(id);
         }
 
-        // DELETE api/<GlassController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpPost("SaveVentilation")]
+        public int SaveVentilation([FromBody] GlassVentilation ventilation)
         {
+            return gMan.SaveVentilation(ventilation);
         }
+
+        [HttpPost("DeleteVentilation")]
+        public ActionResult DeleteVentilation([FromBody] GlassVentilation ventilation)
+        {
+            if (ventilation == null || ventilation.Id < 1)
+                return BadRequest();
+
+            gMan.DeleteVentilation(ventilation.Id);
+
+            return Ok();
+        }
+
+        #endregion
     }
 }
