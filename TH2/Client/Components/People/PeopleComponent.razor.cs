fdd134fd3bd6a092e668fe7f379718a9c44efa48
Menno Bordes <mennobordes@hetnet.nl>
2020-08-20 15:37:09 +0200

    live// Updated using

--- TH2/Client/Components/People/PeopleComponent.razor.cs
+++ TH2/Client/Components/People/PeopleComponent.razor.cs
@@ -3,10 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http.Json;
 
 namespace TH2.Client.Components.People
 {
-    using System.Net.Http.Json;
     using TH2.Shared.Modules.People.Entities;
     public partial class PeopleComponent : ComponentBase
     {
