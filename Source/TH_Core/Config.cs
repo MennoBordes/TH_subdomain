9ce8abd5a03fb48abd081073c82e8260e62e2feb
Menno Bordes <mennobordes@hetnet.nl>
2020-08-25 19:35:01 +0200

    Changed DB

--- Source/TH_Core/Config.cs
+++ Source/TH_Core/Config.cs
@@ -4,10 +4,10 @@
     {
 #if (RELEASE)
         /// <summary> DB connection. </summary>
-        public const string DbConnectionString = "pooling=true;CHARSET=utf8;server=localhost;database=blazor_calc;uid=th;pwd=dbaccess;Allow User Variables=True;"
+        public const string DbConnectionString = "pooling=true;CHARSET=utf8;server=localhost;database=th_calc;uid=th;pwd=dbaccess;Allow User Variables=True;"
 #else
         /// <summary> DB connection. </summary>
-        public const string DbConnectionString = "pooling=true;CHARSET=utf8;server=localhost;database=blazor_calc;uid=root;pwd=admin;Allow User Variables=True;";
+        public const string DbConnectionString = "pooling=true;CHARSET=utf8;server=localhost;database=th_calc;uid=root;pwd=admin;Allow User Variables=True;";
 #endif
 
 
