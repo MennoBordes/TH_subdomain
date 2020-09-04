c8e0dea85cc5556a53ccc652fbe276732f30e822
Menno Bordes <mennobordes@hetnet.nl>
2020-09-04 17:55:04 +0200

    Create new js and css file import methods

--- Source/TH_WebUI/Base/HtmlHelpers/ResourceFactory.cs
+++ Source/TH_WebUI/Base/HtmlHelpers/ResourceFactory.cs
@@ -9,6 +9,42 @@ namespace TH.WebUI.Base.HtmlHelpers
     /// <summary> Html Resource factory. </summary>
     public static class ResourceFactory
     {
+        public static MvcHtmlString Script(this UrlHelper urlHelper, string url)
+        {
+            // Check null
+            if (string.IsNullOrWhiteSpace(url)) return null;
+
+            return new MvcHtmlString("<script type=\"text/javascript\" src=\"" + urlHelper.Content(url) + "\"></script>");
+        }
+
+        public static MvcHtmlString Style(this UrlHelper urlHelper, string url)
+        {
+            // Check Null
+            if (string.IsNullOrWhiteSpace(url)) return null;
+
+            return new MvcHtmlString("<link rel=\"stylesheet\" href=\"" + urlHelper.Content(url) + "\" />");
+        }
+
+        public static MvcHtmlString Script(this HtmlHelper htmlHelper, string url)
+        {
+            // Check null
+            if (string.IsNullOrWhiteSpace(url)) return null;
+
+            UrlHelper urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
+
+            return new MvcHtmlString("<script type=\"text/javascript\" src=\"" + urlHelper.Content(url) + "\"></script>");
+        }
+
+        public static MvcHtmlString Style(this HtmlHelper htmlHelper, string url)
+        {
+            // Check Null
+            if (string.IsNullOrWhiteSpace(url)) return null;
+
+            UrlHelper urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
+
+            return new MvcHtmlString("<link rel=\"stylesheet\" type=\"text/css\" href=\"" + urlHelper.Content(url) + "\" />");
+        }
+
         public static MvcHtmlString THScript(this HtmlHelper htmlHelper, string name)
         {
             // Check null
